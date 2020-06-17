ff755e959ebaccbfcc6fbbbef4eef595d142d68e
Muanya11 <nwokejichimuanya@gmail.com>
2020-06-17 21:30:32 +0100

    first commit

---
+++ app/__init__.py
@@ -0,0 +1,9 @@
+from flask import Flask 
+import os
+
+app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
+
+from app import views
+
+print(os.environ['APP_SETTINGS'])
---
+++ config.py
@@ -0,0 +1,32 @@
+import os 
+
+# set the root directory
+base_dir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+	"""Parent configuration class"""
+	DEBUG = False
+	TESTING =False;
+	CSRF_ENABLED = True
+	SECRET_KEY = "my-secret-key-to-be-set-later"
+
+
+class DevelopmentConfig(Config):
+	"""Config class for development"""
+	DEVELOPMENT = True
+	DEBUG = True
+		
+		
+class StageConfig(Config):
+	"""Config class for staging the app"""
+	DEVELOPMENT = True
+	DEBUG = True
+
+class ProConfig(Config):
+	"""Config class for main production of the app"""
+	DEBUG = False
+
+
+class ProConfig(Config):
+	"""Config class for testing the app"""
+	TESTING = True
